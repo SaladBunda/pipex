445e0c228766b0f557fbb73ee8dc00ed96496312
SaladBunda <khahmaaz@gmail.com>
2024-05-26 22:54:22 +0100

    bonus is working

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ael-maaz <ael-maaz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 22:01:50 by ael-maaz          #+#    #+#             */
-/*   Updated: 2024/05/25 22:50:53 by ael-maaz         ###   ########.fr       */
+/*   Updated: 2024/05/26 22:36:54 by ael-maaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -137,50 +137,48 @@ void init_variables(t_pipx **pipx,int **fork_id, int ***pipe_id, int ac)
 		i++;
 	}
 }
-
 int main(int ac, char **av, char **env)
 {
-	int		count;
-	int outfile = open(av[ac - 1], O_RDWR | O_CREAT | O_TRUNC, 0644);
-	int infile = open(av[1],O_RDONLY);
-	t_pipx *pipx;
-	int *fork_id;
-	int **pipe_id;
-	if (ac > 4)
-	{
-		count = 0;
-		init_variables(&pipx, &fork_id, &pipe_id, ac);
-		init_pipx(pipx, ac);
-		while (count < ac - 3)
-		{
-			pipx[count].pm = check_args(count, av, env, &pipx[count]);
-			if (pipe(pipe_id[count]) == -1)
-				return (perror("pipe\n"), 1);
-			
-			fork_id[count] = fork();
-			if (fork_id[count] == -1)
-			{
-				perror("Fork");
-				exit(EXIT_FAILURE);
-			}
-			if (fork_id[count] == 0)
-			{
+    int count;
+    t_pipx *pipx;
+    int *fork_id;
+    int **pipe_id;
+    int outfile = open(av[ac - 1], O_RDWR | O_CREAT | O_TRUNC, 0644);
+    int infile = open(av[1], O_RDONLY);
+
+    if (ac > 4)
+    {
+        count = 0;
+        init_variables(&pipx, &fork_id, &pipe_id, ac);
+        init_pipx(pipx, ac);
+
+        while (count < ac - 3)
+        {
+            pipx[count].pm = check_args(count, av, env, &pipx[count]);
+
+            if (pipe(pipe_id[count]) == -1)
+                return (perror("pipe\n"), 1);
+
+            fork_id[count] = fork();
+            if (fork_id[count] == -1)
+            {
+                perror("Fork");
+                exit(EXIT_FAILURE);
+            }
+            if (fork_id[count] == 0) // Child process
+            {
 				if (count != 0 && count != ac - 4)
 				{
 					dup2(pipe_id[count - 1][0], STDIN_FILENO);	
 					dup2(pipe_id[count][1], STDOUT_FILENO);	
 					close(pipe_id[count - 1][0]);
 					close(pipe_id[count - 1][1]);
-					close(pipe_id[count][0]);
-					close(pipe_id[count][1]);
 				}
 				else if(count == 0)
 				{
 					dup2(infile,STDIN_FILENO);
 					dup2(pipe_id[count][1],STDOUT_FILENO);
 					close(infile);
-					close(pipe_id[count][0]);
-					close(pipe_id[count][1]);
 				}
 				else
 				{
@@ -189,26 +187,40 @@ int main(int ac, char **av, char **env)
 					close(outfile);
 					close(pipe_id[count -1][1]);
 					close(pipe_id[count -1][0]);
-					close(pipe_id[count][1]);
-					close(pipe_id[count][0]);
 				}
-				
-				if (execve(ft_strjoin_p(pipx[count].cmd[pipx[count].pos], pipx[count].pm[0]), pipx[count].pm,env) == -1)
-				{
-					perror("execv:");
-					exit(EXIT_FAILURE);
-				}
-				exit(EXIT_SUCCESS);
-			}
-			
-			if(count != 0)
-				close(pipe_id[count - 1][1]);
-			close(pipe_id[count][1]);
-			count++;
-		}
-		for(int c = 0;c < ac -3;c++)
-        	while (wait(NULL) > 0);
+
+                close(pipe_id[count][0]);
+                close(pipe_id[count][1]);
+
+                if (execve(ft_strjoin_p(pipx[count].cmd[pipx[count].pos], pipx[count].pm[0]), pipx[count].pm, env) == -1)
+                {
+                    perror("execve");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else // Parent process
+            {
+                if (count != 0)
+                {
+                    close(pipe_id[count - 1][0]);
+                    close(pipe_id[count - 1][1]);
+                }
+                close(pipe_id[count][1]);
+
+                count++;
+            }
+        }
+
+        while (wait(NULL) > 0)
+            ;
+
+        // close(outfile);
+        // close(infile);
 		exit(EXIT_SUCCESS);
-	}
-	exit(EXIT_FAILURE);
+    }
+    exit(EXIT_FAILURE);
 }
+
+
+
+
